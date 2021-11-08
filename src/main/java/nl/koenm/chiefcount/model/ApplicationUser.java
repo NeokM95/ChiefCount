83069b3244e50f2373be4f2a3465520b2bc3e2ee
NeokM95 <koenm95@outlook.com>
2021-11-08 22:12:15 +0100

    created Auth + Authkey model + completed AppUser model with use off Spring Boot starter validation

--- src/main/java/nl/koenm/chiefcount/model/ApplicationUser.java
+++ src/main/java/nl/koenm/chiefcount/model/ApplicationUser.java
@@ -1,20 +1,63 @@
 package nl.koenm.chiefcount.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApplicationUser {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message = "First name cannot be blank")
     private String firstName;
+
+    @NotBlank(message = "Last name cannot be blank")
     private String lastName;
 
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Enter a valid email")
     private String email;
 
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 4, message = "Username must be at least 4 characters")
     private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
+
+    @OneToMany(
+            targetEntity = Authority.class,
+            mappedBy = "username",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.EAGER)
+    private Set<Authority> authorities = new HashSet<>();
+
+    public void addAuthority(Authority authority) {
+        this.authorities.add(authority);
+    }
+    public void addAuthority(String authorityString) {
+        this.authorities.add(new Authority(this.username, authorityString));
+    }
+    public void removeAuthority(Authority authority) {
+        this.authorities.remove(authority);
+    }
+    public void removeAuthority(String authorityString) {
+        this.authorities.removeIf(authority -> authority.getAuthority().equalsIgnoreCase(authorityString));
+    }
 
 }
