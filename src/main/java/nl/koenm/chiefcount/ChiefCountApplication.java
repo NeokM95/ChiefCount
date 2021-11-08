72384e53a8e97a436e622b6138b36e66ceba515a
NeokM95 <koenm95@outlook.com>
2021-11-08 21:06:01 +0100

    setup security + db connection

---
+++ src/main/java/nl/koenm/chiefcount/ChiefCountApplication.java
@@ -0,0 +1,13 @@
+package nl.koenm.chiefcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiefCountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChiefCountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/koenm/chiefcount/ChiefCountApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.koenm.chiefcount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiefCountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
