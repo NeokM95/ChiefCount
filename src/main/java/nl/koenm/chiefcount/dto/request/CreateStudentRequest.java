ce5ed802ed34196c296d04a75ef3d0d63394285e
NeokM95 <koenm95@outlook.com>
2021-11-10 16:50:43 +0100

    created StudentRequest dto

---
+++ src/main/java/nl/koenm/chiefcount/dto/request/CreateStudentRequest.java
@@ -0,0 +1,25 @@
+package nl.koenm.chiefcount.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateStudentRequest {
+
+    @Email(message = "Enter a valid email")
+    private String email;
+
+    @Size(min = 4, max = 20, message = "Enter username between 4-20 characters")
+    private String username;
+
+    @Size(min = 8, max = 20, message = "Password should be at least 8 characters")
+    private String password;
+}
